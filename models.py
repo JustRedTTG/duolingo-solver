93afe4625dc5bf8d51b7ac394194c6117768badb
red <red@redttg.com>
2024-03-25 07:03:49 +0200

    use .env for db string

--- models.py
+++ models.py
@@ -1,11 +1,17 @@
+import os
+
 from sqlalchemy import create_engine, Column, Integer, String
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 Base = declarative_base()
 
-engine = create_engine('sqlite:///db', echo=True)
+engine = create_engine(os.environ.get('DATABASE_CONNECTION_STRING'), echo=True)
 
 
 class QuestionAnswer(Base):
